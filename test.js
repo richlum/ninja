4ac741ae57a79769fb7169121e891c47e8677bab
Richard Lum <richardlum13@gmail.com>
2015-06-14 17:54:59 -0700

    midpoint testing assert

--- test.js
+++ test.js
@@ -1,4 +1,6 @@
+
 window.onload=function() {
+		var assert = getAssert();
 		var base = document.getElementById("basic");
 		var li = document.createElement("li");
 		li.className = "hello world";
@@ -7,6 +9,8 @@ window.onload=function() {
 		color = "green";
 		var myFunc = outer("arg1");
 		myFunc();
+		testNinja();
+		
 };
 
 var outer = function (anarg) {
@@ -21,6 +25,65 @@ var outer = function (anarg) {
 		return displayName;
 	}();
 };
+
+var results;
+var getAssert = function() {
+	return function makeassert(){
+		function myassert (value, descr) {
+			var li = document.createElement("li");
+			li.className = value ? "pass" : "fail";
+			li.appendChild(document.createTextNode(desc));
+			console.log("results = " + results);
+			results.appendChild(li);
+		}
+		return myassert;
+	}();
+};
+
+function testNinja(){
+	var ninja = {
+			chirp : function(n) {
+				return n > 1 ? ninja.chirp(n-1) + "-chirp" : "chirp";
+			}
+	};
+	console.log(ninja.chirp(4));
+	showtype(this,'object');  // 'function' 'object' 'undefined' 'string' 'number' 'null' 
+	assert(ninja.chirp(2),"chirp-chirp");
+	var samarai = { chirp: ninja.chirp };
+
+	//ninja = {};
+
+	try {
+		assert(samarai.chirps(4) = "chirp-chirp-chirp-chirp",
+			"Does this work?");
+	}
+	catch(e) {
+			//assert(false,
+			//	"This doesnt work");
+	}
+};
+
+function showtype(that, what) {
+	var cnt = 0;
+	for (it in that){
+		//if  ((typeof that[it]) === what )  {
+		console.log( cnt++ + " " + 
+						it + ":" + 
+						typeof that + ";" + 
+						typeof that[it] + " : " + 
+						that[it]);
+		//}
+	}
+};
+
+function assert (value, descr) {
+	var li = document.createElement("li");
+	li.className = value ? "pass" : "fail";
+	li.appendChild(document.createTextNode(descr));
+	console.log("results = " + results);
+	results.appendChild(li);
+}
+
 
 
 
